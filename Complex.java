0b7c7999b07193c30eac6dfb1e42d42b897c5401
AI-moc <2539804079@qq.com>
2020-12-14 20:00:21 +0800

    添加了减法和除法函数

--- Complex.java
+++ Complex.java
@@ -11,6 +11,18 @@ class Complex
 		double sum =real+imag; 
 		System.out.println("sum=" + sum);
 	}
+	public void subtraction()
+	{
+		double diffence =real-imag; 
+		System.out.println("diffence=" + diffence);
+	}
+	public void multiplication()
+	{
+		double product =real*imag; 
+		System.out.println("product=" + product);
+	}
+	
+
 	public Complex(double real,double imag)
 	{
 		this.real = real;
@@ -20,5 +32,8 @@ class Complex
 	{
 		Complex c = new Complex(1.5,2.5);
 		c.add();
+		c.subtraction();
+		c.multiplication();
+		
 	}
 }
